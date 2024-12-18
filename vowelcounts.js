91599a26618c89079165e946aa8733e73a0b3358
minakdr <mina125.ak@gmail.com>
2024-12-18 10:11:09 +0100

    nthg

--- vowelcounts.js
+++ vowelcounts.js
@@ -1,16 +1,7 @@
-console.log("Hello JavaScript");
 function getCount(str) {
     let total = 0;
     let array = str.split("");
     console.log(array);
-
-
-
-
-    array.map((el) => {
-        return 
-    })
-
     for ( let i = 0 ; i < array.length; i++) {
       switch (str[i]) {
         case 'a':
