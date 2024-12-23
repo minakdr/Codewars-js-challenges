d55daf8c441d3f2ebc5e9421b369a8a9fecced6f
minakdr <mina125.ak@gmail.com>
2024-12-23 11:05:05 +0100

    h

--- xo.js
+++ xo.js
@@ -1,7 +1,19 @@
 function XO(str) {
 str=str.toLowerCase()
+array=str.split('')
+l=array.length
+let cx = 0 ;
+let co = 0 ; 
+for ( let i = 0; i<l ; i++){
+    if (array[i] == 'x') {cx++}
+    else if (array[i]=='o'){co++}
+}
 
-
-
+console.log(co)
+console.log(cx)
+if (co==cx) { return true}
+else { return false }
 
 }
+
+console.log(XO("ooxx"));
